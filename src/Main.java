811be3af61254af7376525b4b0cb67f2fc347019
fernando-daw <flagonovoa@danielcastelao.org>
2022-12-16 03:26:24 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       // Scanner espacio = new Scanner(System.in);
-       // System.out.println("Indica el tamaño del parking");
-        //int tamaño = espacio.nextInt();
+        Scanner cubrir = new Scanner(System.in);
 
-        //Parking nuevo = new Parking(tamaño);
+        System.out.println("¿De cuantas plazas dispones tu parking?");
+        int plazas = cubrir.nextInt();
+        Parking parking = new Parking(plazas);
+        System.out.println("¿Como quieres que sea la entrada de vehiculos? \n1 Manual \n2 Aleatoria ");
+        int respuesta = cubrir.nextInt();
+        vehiculo[] coches = parking.rellenarparking(respuesta);
+        boolean salida= false;
+        do {
+            System.out.println("¿Que deseas hacer? \n1 Mostrar todos los registros del parking. \n2 Buscar vehiculos por plazas. \n3 Buscar vehiculos por matricula. \n4 Retirar un vehiculo del parking. \n5 Salir del programa. ");
+            int menu = cubrir.nextInt();
 
+            switch (menu) {
 
-        Scanner aparcamiento = new Scanner(System.in);
-        System.out.println("Seleccione el tipo de aparcamiento:\n1 Manual \n2 Automático");
-        int respuesta2 = aparcamiento.nextInt();
+                case 1:
+                    parking.verparking(coches);
+                    break;
+                case 2:
+                    parking.encontrarcocheindice(coches);
+                    break;
+                case 3:
+                    parking.encontrarcoche(coches);
+                    break;
+                case 4:
+                    parking.quitar_vehiculo(coches);
+                    break;
+                case 5:
+                    System.out.println("Fin del programa");
+                    return;
+            }
 
-
-
-
+        } while (salida = true);
 
 
 
--- src/vehiculo.java
+++ src/vehiculo.java
@@ -14,20 +14,53 @@ public class vehiculo {
         if (tamaño==2){
             tipo="camion";
         }
-        letrasMatricula();
+        genera_matricula();
     }
 
-     //funcion que crea las matrículas de forma aleatoria
-    public  String  letrasMatricula (){
-        Random ale2 = new Random();
-        int aleatorio2= ale2.nextInt(9999)+1000;
-        String [] arrayletras ={"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"};
-        for (int i=0; i<3;i++){
-            Random ale = new Random();
-            int aleatorio = ale.nextInt(arrayletras.length);
-            matricula = aleatorio2 + arrayletras[aleatorio];
-        }
-        return matricula ;
+    public String toString() {
+        return " el vehiculo introducido es " + tipo + "con matricula = " + matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    //funcion que crea las matrículas de forma aleatoria
+    public  String  genera_matricula (){
+        String[] letras = new String[]{"B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"};
+        int numeros_matricula = (int) Math.floor(Math.random() * (9999 - 999 + 1) + 999);
+
+        int letra = (int) Math.floor(Math.random() * ((letras.length - 1) - 0 + 1) + 0);
+        int letra2 = (int) Math.floor(Math.random() * ((letras.length - 1) - 0 + 1) + 0);
+        int letra3 = (int) Math.floor(Math.random() * ((letras.length - 1) - 0 + 1) + 0);
+        String indiv = "";
+        indiv = letras[letra];
+        String indiv2 = "";
+        indiv2 = letras[letra2];
+        String indiv3 = "";
+        indiv3 = letras[letra3];
+        matricula = numeros_matricula + indiv + indiv2 + indiv3;
+        return matricula;
      }
 
 
